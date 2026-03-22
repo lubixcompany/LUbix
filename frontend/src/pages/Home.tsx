e8bfda5191f6a8695099e10730b96d083880d102
nicolas <johannicolasad@gmail.com>
2026-03-22 15:06:38 -0500

    Proyecto fullstack inicial (frontend + backend)

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white p-10">
+      <h1 className="text-4xl font-bold mb-6">
+        Bienvenida a mi proyecto 🚀
+      </h1>
+
+      <p className="text-lg text-gray-300">
+        Este es el frontend conectado con mi backend en FastAPI
+      </p>
+    </div>
+  )
+}
+
+export default Home
